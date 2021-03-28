70f4269ff31332c93f4d048814ede684bc4fe1c4
Kenneth Coffie <geniounico@outlook.com>
2021-03-28 20:10:05 +0100

    minor changes

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,11 @@
-import { Box, Text, Flex } from "@chakra-ui/react";
+import { chakra, Flex } from "@chakra-ui/react";
 
 
 function Footer() {
   return (
     <Flex as='footer' borderTop='1px' borderTopColor='gray.200' justifyContent='center' alignItems='center' height={100}>
-      <Box textAlign='center'>Copyright 2020</Box>
+      <chakra.a href='https://tailwindui.com/img/components/home-screens.02-card-layout-with-sidebar-xl.png' pr={3}>Design by TailwindUI</chakra.a>
+      <chakra.span textAlign='center'>Copyright Kenny Coffie 2020</chakra.span>
     </Flex>
   );
 }
